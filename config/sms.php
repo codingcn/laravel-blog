1c66499e7d0c352774f8d18660fa47ba238ea123
Alan <5303221@gmail.com>
2018-02-21 22:37:46 +0800

    sms

--- config/sms.php
+++ config/sms.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'ali'=>[
-        'client_id'=>env('GITHUB_CLIENT_ID', ''),
-        'client_secret'=>env('GITHUB_CLIENT_SECRET', '')
+        'sms_app_key'=>env('SMS_APP_KEY', ''),
+        'sms_secret_key'=>env('SMS_SECRET_KEY', '')
     ]
 ];
